3924f6f5535aff23d89d19ff61476af57b609fcb
MiguelGuevara25 <miguel_g1789@hotmail.com>
2024-08-29 22:59:58 -0500

    seguir en compu

--- src/components/RespiroVisual.jsx
+++ src/components/RespiroVisual.jsx
@@ -1,8 +1,8 @@
 const RespiroVisual = () => {
   return (
-    <section className="bg-[#45260B] h-screen flex flex-col justify-center items-center relative font-bold text-[#FFFAF3] [&>p]:text-[#FFFAF3]/20 text-7xl [&>p]:absolute [&>p]:text-7xl [&>p]:font-extrabold">
+    <section className="bg-[#45260B] h-screen flex flex-col justify-center items-center relative font-bold text-[#FFFAF3] [&>p]:text-[#FFFAF3]/20 md:text-7xl text-5xl [&>p]:absolute [&>p]:text-7xl [&>p]:font-extrabold">
       <div
-        className="max-w-md mb-5 relative z-50"
+        className="md:max-w-md max-w-60 lg:mb-5 mb-10 relative z-50"
         data-aos="zoom-in-up"
         data-aos-duration="1500"
       >
@@ -16,14 +16,16 @@ const RespiroVisual = () => {
       <div>
         <h3 className="text-center">
           Pasado que <br />
-          <span className="text-8xl">inspira</span>
+          <span className="md:text-8xl text-6xl">inspira</span>
         </h3>
       </div>
 
       <p className="float__words__inverse__1 top-56 right-32 transition-all">
         Herencia
       </p>
-      <p className="float__words__1 top-72 left-32 transition-all">Innovación</p>
+      <p className="float__words__1 top-72 left-32 transition-all">
+        Innovación
+      </p>
       <p className="float__words right-16 bottom-48 transition-all">
         Tradicionales
       </p>
